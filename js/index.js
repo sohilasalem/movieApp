b1105d71b8f6bbd9443d171d55052cdcde4eadcb
sohilasalem1123@gmail.com <sohilasalem1123@gmail.com>
2024-07-26 14:53:54 +0300

    jjj

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 var rowData = document.getElementById("rowData");
 var searchInput = document.getElementById("searchInput");
 var animateanimated = document.getElementById("animate__animated");
-// var p = document.getElementById("p");
 var newplaying = document.getElementById("newplaying");
 var airtoday = document.getElementById("airtoday");
 var onair = document.getElementById("onair");
@@ -9,29 +8,49 @@ var popular = document.getElementById("popular");
 var toprated = document.getElementById("toprated");
 var trendy = document.getElementById("trendy");
 var upcoming = document.getElementById("upcoming");
-const inputs = document.querySelectorAll("input");
+var inputs = document.querySelectorAll("input");
+var UsertEmail=document.getElementById('inputEmail');
+var inputError=document.getElementById('inputError');
+
+
 
 
 
 function openSideNav() {
-    $(".side-nav-menu").animate({
+$(".side-nav-menu").animate({
         left: 0
-    }, 500)
-
-
-    $(".open-close-icon").removeClass("fa-align-justify ");
-    $(".open-close-icon").addClass("fa-xmark");
-
-
-    for (let i = 0; i < 8; i++) {
-        $(".links li").eq(i).animate({
-            top: 0
-        }, (i + 6) * 100)
-    }
-    // $(".links li").animate({
-    //     top: 0
-    // }, 1000)
+}, 500)
+$(".open-close-icon").removeClass("fa-align-justify ");
+$(".open-close-icon").addClass("fa-xmark");
+for (let i = 0; i < 8; i++) {
+$(".links li").eq(i).animate({
+  top: 0
+}, (i + 6) * 100)
 }
+}
+//////////////////////////////////////////
+$(window).scroll(function(){
+    // console.log("hiu");
+    // var btn=
+var top=  $(window).scrollTop()
+    // console.log(top);
+    if(top > 800){
+$('#btn-up').show()
+
+    }else{
+$('#btn-up').hide()
+
+    }
+})
+
+$('#btn-up').click(function(){
+    $(window).scrollTop(0)
+    $('html,body').animate({
+    scrollTop:0
+    },400)
+  })
+
+  ///////////////////////////////////////////
 
 function closeSideNav() {
     let boxWidth = $(".side-nav-menu .nav-tab").outerWidth()
@@ -72,7 +91,6 @@ if (term.trim() === "") {
 
 /////////////////////////////////////////////////////
 
-// https://api.themoviedb.org/3/search/movie?query=Jack+Reacher&api_key=ba8b9a7199efdcb0ca1f96879b83c44
 
 async function getMovies() {
     rowData.innerHTML=""
@@ -271,73 +289,192 @@ toprated.addEventListener("click", function (e) {
     getTopRatedMovies();
  });
 
-/////////////////////////////////
-// const p = document.getElementById("p");
-// p.addEventListener("mouseenter", function (e) {
-// // $("p").css("dispaly","block")
-// // p.classList.replace("d-none", "d-block");
-// document.querySelector(".mmm").replace("d-none", "d-block");
 
+//  ////////////////////////
+var Userpass=document.getElementById('inputPass');
+var UserRepass=document.getElementById('inputRepass');
+var Userage=document.getElementById('inputAge');
+var Userphone=document.getElementById('inputPhone');
+var inputError=document.getElementById('inputError');
 
-  
-//  });
+let nameInputTouched = false;
+let emailInputTouched = false;
+let phoneInputTouched = false;
+let ageInputTouched = false;
+let passwordInputTouched = false;
+let repasswordInputTouched = false;
 
-// var inputError=document.getElementById('inputError');
-// formData.addEventListener("input", function () {
-//     if (validationEmail() && validationPassword()) {
-//        isValid = true;
-//     } else {
-//        isValid = false;
-//     }
-//  });
-
-// function validationEmail() {
-//     const regexStyle =
-//        /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
- 
-//     if (regexStyle.test(inputs[0].value)) {
-//        // el tmam
-//        inputs[0].classList.add("is-valid");
-//        inputs[0].classList.remove("is-invalid");
-//        return true;
-//     } else {
-//        //el mesh tmam
- 
-//        inputs[0].classList.add("is-invalid");
-//        inputs[0].classList.remove("is-valid");
- 
-//        return false;
-//     }
-//  }
-
-
- ////////////////////////
-
-
-//  let homeHeight = $('#demoo').height(); // height home section
-
-$(window).scroll(function(){
-    // console.log("hiu");
-    // var btn=
-var top=  $(window).scrollTop()
-    // console.log(top);
-    if(top > 800){
-$('#btn-up').show()
-        // $('.btnUp').addClass('.d-block')
-// $('.d-none').show(1000)
-
-    }else{
-        // $('.btnup').addClass('.d-none')
-$('#btn-up').hide()
-
-
-    }
+function inputsValidation(){
+    document.getElementById("UsertEmail").addEventListener("focus", () => {
+    emailInputTouched = true
 })
 
-$('#btn-up').click(function(){
-    $(window).scrollTop(0)
-    $('html,body').animate({
-    scrollTop:0
-    },400)
-  })
-  
+document.getElementById("Userphone").addEventListener("focus", () => {
+    phoneInputTouched = true
+})
+
+document.getElementById("Userage").addEventListener("focus", () => {
+    ageInputTouched = true
+})
+
+document.getElementById("Userpass").addEventListener("focus", () => {
+    passwordInputTouched = true
+})
+
+document.getElementById("UserRepass").addEventListener("focus", () => {
+    repasswordInputTouched = true
+})
+
+
+
+const button = document.getElementById('btn');
+let moved = false;
+
+
+    if (nameInputTouched) {
+        if (nameValidation()) {
+            document.getElementById("UsertName").classList.replace("d-block", "d-none")
+            $('.form-control').css('border-color','#fff')
+        } else {
+            document.getElementById("UsertName").classList.replace("d-none", "d-block")
+            $('.form-control').css('border-color','#DC3545')
+        }
+    }
+    
+    if (emailInputTouched) {
+
+        if (emailValidation()) {
+            document.getElementById("inputEmail").classList.replace("d-block", "d-none")
+            $('.form-control').css('border-color','#fff')
+        } else {
+            document.getElementById("inputEmail").classList.replace("d-none", "d-block")
+            $('.form-control').css('border-color','#DC3545')
+
+        }
+    }
+
+    if (phoneInputTouched) {
+        if (phoneValidation()) {
+            document.getElementById("inputPhone").classList.replace("d-block", "d-none")
+            $('.form-control').css('border-color','#fff')
+        } else {
+            document.getElementById("inputPhone").classList.replace("d-none", "d-block")
+            $('.form-control').css('border-color','#DC3545')
+
+        }
+    }
+
+    if (ageInputTouched) {
+        if (ageValidation()) {
+            document.getElementById("inputAge").classList.replace("d-block", "d-none")
+            $('.form-control').css('border-color','#fff')
+        } else {
+            document.getElementById("inputAge").classList.replace("d-none", "d-block")
+            $('.form-control').css('border-color','#DC3545')
+        }
+    }
+
+    if (passwordInputTouched) {
+        if (passwordValidation()) {
+            document.getElementById("inputPass").classList.replace("d-block", "d-none")
+            $('.form-control').css('border-color','#fff')
+        } else {
+            document.getElementById("inputPass").classList.replace("d-none", "d-block")
+            $('.form-control').css('border-color','#DC3545')
+
+        }
+    }
+    if (repasswordInputTouched) {
+        if (repasswordValidation()) {
+            document.getElementById("inputRepass").classList.replace("d-block", "d-none")
+            $('.form-control').css('border-color','#fff')
+        } else {
+            document.getElementById("inputRepass").classList.replace("d-none", "d-block")
+            $('.form-control').css('border-color','#DC3545')
+
+        }
+    }
+
+
+  if(   nameValidation() == false ||
+  emailValidation() == false ||
+  phoneValidation() == false ||
+  ageValidation() == false ||
+  passwordValidation() == false ||
+  repasswordValidation() == false) {
+    console.log('no');
+
+    $('.form-btn').addClass('shake-button')
+    $('.form-btn').addClass('invalid')
+    button.addEventListener('mouseover', () => {
+        if (!moved) {
+            $('.form-btn').addClass('ttn')
+            button.style.left = '200px';
+        } 
+        
+    });
+    button.addEventListener('mouseover', () => {
+        if (moved) {
+            button.style.left = '0px';
+            $('.form-btn').addClass('ttn')
+        } 
+        moved = !moved;
+    });
+
+  }
+  else{
+    $('.form-btn').removeClass('invalid')
+    $('.form-btn').removeClass('shake-button')
+$('.form-btn').removeClass('ttn')
+
+    button.addEventListener('mouseover', () => {
+        if (!moved) {
+            $('.form-btn').removeClass('ttn')
+            button.style.left = '0px';
+        } 
+        
+    });
+
+    button.addEventListener('mouseover', () => {
+        if (moved) {
+            button.style.left = '0px';
+            $('.form-btn').removeClass('ttn')
+        } 
+        else {
+            button.style.left = '200px';
+        }
+        moved = !moved;
+    });
+    // $('.form-btn').removeClass('ttn')
+  }
+
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+function nameValidation() {
+    return (/^[a-zA-Z ]+$/.test(document.getElementById("UsertName").value))
+    
+}
+
+
+
+function emailValidation() {
+    return (/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{3}))$/.test(document.getElementById("UsertEmail").value))
+}
+
+function phoneValidation() {
+    return (/^01\d{8}$/.test(document.getElementById("Userphone").value))
+}
+
+function ageValidation() {
+    return (/^(1[6-9]|[2-9][0-9]|1[0-9]{2,})$/.test(document.getElementById("Userage").value))
+}
+
+function passwordValidation() {
+    return (/^(?=.*\d)(?=.*[a-z])[0-9a-zA-Z]{8,}$/.test(document.getElementById("Userpass").value))
+}
+
+function repasswordValidation() {
+    return document.getElementById("UserRepass").value == document.getElementById("Userpass").value
+} 
